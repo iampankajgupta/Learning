349385a51746e129c1bc39e6c3e6ea91ffe5b313
Piyush Gupta <piyushpappugupta@gmail.com>
2023-02-11 21:36:13 +0530

    design pattern changes added

--- src/main/java/StrategyDesignPattern/VehicleExample/Vehicle.java
+++ src/main/java/StrategyDesignPattern/VehicleExample/Vehicle.java
@@ -1,22 +1,13 @@
 package StrategyDesignPattern.VehicleExample;
 
+import StrategyDesignPattern.VehicleExample.Strategy.DriveStrategy;
+
 public class Vehicle {
-
   DriveStrategy driveStrategy;
-
   public Vehicle(DriveStrategy driveStrategy) {
     this.driveStrategy = driveStrategy;
   }
-
   public void drive(){
-    driveStrategy.drive();;
-  }
-
-  public DriveStrategy getDriveStrategy() {
-    return driveStrategy;
-  }
-
-  public void setDriveStrategy(DriveStrategy driveStrategy) {
-    this.driveStrategy = driveStrategy;
+    driveStrategy.drive();
   }
 }
